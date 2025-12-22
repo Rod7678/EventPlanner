7d9a5352f2bd851dd75b27aa424eb5aafaaba6d8
Rohit <rohitghadge348@gmail.com>
2025-12-22 12:38:47 +0530

    small error

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -17,7 +17,7 @@ function MainNavigation() {
             <NavLink to='/newsletter' className={({isActive}) => isActive ? classes.active : undefined}>Newsletter</NavLink>
           </li>
           <li>
-            <NavLink to='/auth' className={({isActive}) => isActive ? classes.active : undefined}>Authentication</NavLink>
+            <NavLink to='/auth?mode=login' className={({isActive}) => isActive ? classes.active : undefined}>Authentication</NavLink>
           </li>
         </ul>
       </nav>
